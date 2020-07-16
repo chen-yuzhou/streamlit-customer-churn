8e5994ae3d6564de9fdf19e12f9280dab51e69f8
chen-yuzhou <chen.yuzhou@wustl.edu>
2020-07-16 18:18:24 -0400

    update setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 mkdir -p ~/.streamlit/
+
 echo "\
 [general]\n\
 email = \"chenyuzh1991@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+"
